4c261861a06ea8209f7b8bd5538db651b541c334
NizarKazze <niaro2005@gmail.com>
2026-04-17 14:07:31 +0200

    Style fixes and img names

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,10 +5,10 @@ const NavBar = () => {
     <div id="navbar-content" className="p-4 flex justify-center">
         <nav className="w-fit">
             <ul className="flex justify-center gap-4 sm:gap-8 bg-white/10 backdrop-blur-lg border border-white/20 rounded-4xl py-4 px-8 text-sm">
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#about">About me</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#skills" className="whitespace-nowrap">Skills</a></li>
+            <li><a href="#about" className="whitespace-nowrap">About me</a></li>
+            <li><a href="#projects" className="whitespace-nowrap">Projects</a></li>
+            <li><a href="#contact" className="whitespace-nowrap">Contact</a></li>
             </ul>
         </nav>
     </div>
